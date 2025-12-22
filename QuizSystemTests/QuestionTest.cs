c175fcb6c6720de4752fdd6da6b6b680cfa1e332
ConorWilsonUU <Wilson-C90@ulster.ac.uk>
2025-12-22 21:13:15 +0000

    Add comments to QuestionTest methods for clarity

--- QuizSystemTests/QuestionTest.cs
+++ QuizSystemTests/QuestionTest.cs
@@ -9,6 +9,7 @@ namespace QuestionTests
         public void QuestionConstructor_ShouldInitialiseProperties()
         {
             // Arrange
+            // Add known values to pass into contructor
             int questionID = 1;
             string questionText = "This is a question";
             string questionCorrectAnswer = "A";
@@ -16,9 +17,11 @@ namespace QuestionTests
             string questionOptions = "A, B, C";
 
             // Act
+            // Create a new Question object using the arranged values
             Question test = new Question(questionID, questionText, questionOptions, questionCorrectAnswer, questionDifficulty);
 
             // Assert
+            // Verify that contructor correctly assigned values to properties
             Assert.AreEqual(questionID, test.QuestionID);
             Assert.AreEqual(questionText, test.QuestionText);
             Assert.AreEqual(questionCorrectAnswer, test.QuestionCorrectAnswer);
@@ -30,13 +33,16 @@ namespace QuestionTests
         public void TextProperty_ShouldGetAndSet()
         {
             // Arrange
+            // Create a question object with initial values
             Question test = new Question(1, "This is a question", "A", "Easy", "A, B, C");
             String newQuestionText = "Is this a question";
 
             // Act
+            // Update the QuestionText property
             test.QuestionText = newQuestionText;
 
             // Assert
+            // Confirm the property was updated correctly
             Assert.AreEqual(newQuestionText, test.QuestionText);
         }
     }
